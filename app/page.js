d34427929cc1fa3afebe48e0907fd01192f20eb2
Noah Pittman <noahpittman00@gmail.com>
2023-10-31 17:38:09 -0400

    fix burger

--- app/page.js
+++ app/page.js
@@ -77,7 +77,7 @@ function Navbar() {
 			<div className="fixed z-10 md:hidden">
 				<Sheet>
 					<SheetTrigger>
-						<div className="border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors w-fit focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+						<div className="border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
 							<HamburgerMenuIcon className="h-4 w-4" />
 						</div>
 					</SheetTrigger>
