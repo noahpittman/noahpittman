a16660f7b7457e96755ab0e74020b4afd8840f32
Noah Pittman <noahpittman00@gmail.com>
2023-11-11 11:30:52 -0500

    update sectionheading for mobile

--- components/SectionHeading.jsx
+++ components/SectionHeading.jsx
@@ -4,13 +4,17 @@ const SectionHeading = ({ children, scrollto }) => {
 	return (
 		<h2 className="text-4xl font-semibold">
 			{children}{" "}
-			<Link href={`/#${scrollto}`} scroll className="opacity-50 text-2xl pl-2">
+			<Link
+				href={`/#${scrollto}`}
+				scroll
+				className="opacity-50 text-2xl pl-2 hidden sm:inline-block"
+			>
 				#
 			</Link>{" "}
 			<Link
 				href={`/#home`}
 				scroll
-				className=" opacity-40 font-normal underline text-sm"
+				className=" opacity-40 font-normal underline text-sm hidden sm:inline-block"
 			>
 				Back to top
 			</Link>
